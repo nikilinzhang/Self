9a0f27f2cdb3ff97f014d359e727474163034752
nikilinzhang <niki.linzhang@gmail.com>
2018-08-26 20:12:44 +1000

    update home page add instagram url to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180825075623) do
+ActiveRecord::Schema.define(version: 20180826092841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180825075623) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "instagram"
   end
 
   add_foreign_key "educations", "users"
